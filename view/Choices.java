f4db6709bdc923698595ed52155158a3bd8603f5
Madhuri Bandham <bandhammadhuri@gmail.com>
2017-03-08 10:59:04 +0530

    Update Choices.java
    Checking for user input

--- view/Choices.java
+++ view/Choices.java
@@ -1,16 +1,51 @@
-package com.full.shopping.view;
+package com.full.shopping;
 
 import java.util.Scanner;
 
 public class Choices {
-	Scanner scan=new Scanner(System.in);
+	String userInput, choice;
+	Scanner scan = new Scanner(System.in);
+
 	public void options() {
-		System.out.println("\t\t 1.show Products \n\t\t 2.Customer Purchased List \n\t\t 3.exit ");
+		System.out.println("\t\t 1.store Products \n\t\t 2. Add Product\n\t    3.Customer Purchased List \n\t\t    4.exit ");
+		System.out.println("\n\t\tChoose your option\n");
 	}
-	public int getChoice() {
+
+	public String getChoice() {
+
+		choice = scan.next();
+		return choice;
+
+	}
+
+	public String inputCheck() {
 		
-		int choice = scan.nextInt();
+			String choice1 = scan.next();
+do{
+			switch (choice1.toLowerCase()) {
+			case "yes":
+				choice = choice1;
+				break;
+			case "no":choice=choice1;
+				break;
+			default:
+				System.out.println("enter valid input(yes/no)");
+				choice = scan.next();
+				break;
+			}}while(!(choice.equalsIgnoreCase("yes"))&&!(choice.equalsIgnoreCase("no")));
+		
 		return choice;
 	}
-	
+
+	/*
+	 * public String inputCheck() throws Exception { userInput=scan.next(); try
+	 * { if
+	 * ((userInput.equalsIgnoreCase("yes"))||(scan.next().equalsIgnoreCase("no")
+	 * )) { return userInput;
+	 * 
+	 * } else throw new Exception(); } catch (Exception e) {
+	 * System.out.println("Enter Valid Input"); }
+	 * 
+	 * return userInput; }
+	 */
 }

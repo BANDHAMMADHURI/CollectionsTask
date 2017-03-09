0788161bb4404ec011ccfe2449446ce4ffef0d7f
Madhuri Bandham <bandhammadhuri@gmail.com>
2017-03-09 17:26:51 +0530

    Update ShoppingCart.java

--- controller/ShoppingCart.java
+++ controller/ShoppingCart.java
@@ -11,15 +11,15 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class ShoppingCart {
-	
-	static String  customerName, item;
+
+	static String customerName, item;
 	static int availability;
 	static String customerInput, userTotalItems;
 	static Integer itemQuantity;
 	static Map<String, Integer> customerItems;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static List<String> customersList = new LinkedList<String>();
-	static Map<String, Integer> storeProducts = new TreeMap<String, Integer>();
+	static Map<String, Integer> storeProducts = new TreeMap<String, Integer>(/*String.CASE_INSENSITIVE_ORDER*/);
 	static Choices choice = new Choices();
 
 	public static void main(String[] args) throws Exception {
@@ -28,8 +28,8 @@ public class ShoppingCart {
 		storeProducts.put("bread", 13);
 		storeProducts.put("sugar", 13);
 		storeProducts.put("coffee", 13);
-		storeProducts.put("toothpaste", 13);
-		storeProducts.put("toothBrush", 13);
+		storeProducts.put("lays", 13);
+		
 
 		out.println("\t====Welcome to ShoppingCart====\n");
 		do {
@@ -42,7 +42,7 @@ public class ShoppingCart {
 				customerInput = choice.inputCheck();
 				break;
 			case "2":
-				customerItems = new TreeMap<String, Integer>();
+				customerItems = new TreeMap<String, Integer>(/*String.CASE_INSENSITIVE_ORDER*/);
 
 				nameCheck();
 				userItems();
@@ -100,10 +100,9 @@ public class ShoppingCart {
 						if (itemQuantity > availability)
 							itemQuantity = 0;
 						availability = availability - itemQuantity;
-						itemQuantity = itemQuantity + customerItems.get(item);
-
+						itemQuantity = itemQuantity + customerItems.get(item)+1;
+						System.out.println(itemQuantity);
 						customerItems.put(item, itemQuantity);
-
 						storeProducts.put(item, availability);
 
 					}
@@ -118,7 +117,7 @@ public class ShoppingCart {
 				customerInput = choice.inputCheck();
 
 				break;
-			
+
 			case "no":
 				break;
 			default:
@@ -136,7 +135,7 @@ public class ShoppingCart {
 			if (availability >= itemQuantity) {
 
 				availability = availability - itemQuantity;
-				
+
 				customerItems.put(item, itemQuantity);
 				storeProducts.put(item, availability);
 			} else {
@@ -148,11 +147,9 @@ public class ShoppingCart {
 
 		catch (Exception e) {
 			out.println("out of stock\n");
-			/*out.println("do you want to purchase any other item(yes/no)");
-			customerInput=choice.inputCheck();
-			if(customerInput.equals("yes"))*/
-					userItems();
 			
+			userItems();
+
 		}
 	}
 
@@ -164,7 +161,7 @@ public class ShoppingCart {
 			for (int i = 0; i < customerName.length(); i++) {
 				b = Character.isLetter(customerName.charAt(i));
 				if (b)
-					continue;
+					continue; 
 				else {
 					out.println("enter in string format only");
 					break;
@@ -178,14 +175,9 @@ public class ShoppingCart {
 			customerInput = "no";
 			try {
 				out.println("enter quantity");
-				// quantityMatchCheck();
+
 				itemQuantity = Integer.parseInt(br.readLine());
-				/*
-				 * availability = storeProducts.get(item); remain =
-				 * availability;
-				 * 
-				 * itemQuantityCheck(availability, itemQuantity);
-				 */
+
 			} catch (Exception e) {
 				out.println("enter digits only");
 				out.println("do you want to again...(yes/no)");

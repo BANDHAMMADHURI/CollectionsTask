0ee7281c337c8d80af38021f3b7f4bc32354db8f
Madhuri Bandham <bandhammadhuri@gmail.com>
2017-03-09 22:43:20 +0530

    Update ShoppingCart.java

--- controller/ShoppingCart.java
+++ controller/ShoppingCart.java
@@ -5,11 +5,10 @@ import static java.lang.System.out;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.InputMismatchException;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class ShoppingCart {
 
@@ -20,11 +19,7 @@ public class ShoppingCart {
 	static Map<String, Integer> customerItems;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static List<String> customersList = new LinkedList<String>();
-	static Map<String, Integer> storeProducts = new TreeMap<String, Integer>(/*
-																				 * String
-																				 * .
-																				 * CASE_INSENSITIVE_ORDER
-																				 */);
+	static Map<String, Integer> storeProducts = new LinkedHashMap<String, Integer>();
 	static Choices choice = new Choices();
 
 	public static void main(String[] args) throws Exception {
@@ -46,18 +41,16 @@ public class ShoppingCart {
 				customerInput = choice.inputCheck();
 				break;
 			case "2":
-				customerItems = new TreeMap<String, Integer>(/*
-																 * String.
-																 * CASE_INSENSITIVE_ORDER
-																 */);
-
+				customerItems = new LinkedHashMap<String, Integer>();
 				nameCheck();
 				userItems();
-
-				out.println("Customer :" + customerName + " \npurchased items");
-				out.println(customerItems);
-				userTotalItems = customerName.concat((customerItems).toString());
-				customersList.add(userTotalItems);
+				if (!customerItems.isEmpty()) {
+					out.println("Customer :" + customerName + " \npurchased items");
+					out.println(customerItems);
+					out.println("Thank You for Choosing Us....Visit Again");
+					userTotalItems = customerName.concat((customerItems).toString());
+					customersList.add(userTotalItems);
+				}
 				out.println("display menu(yes/no)");
 				customerInput = choice.inputCheck();
 				break;
@@ -93,7 +86,7 @@ public class ShoppingCart {
 					if (!customerItems.containsKey(item)) {
 						if (storeProducts.containsKey(item)) {
 							quantityMatchCheck();
-							
+
 							availability = storeProducts.get(item);
 							itemQuantityCheck(availability, itemQuantity);
 						} else {
@@ -102,9 +95,12 @@ public class ShoppingCart {
 					} else {
 						quantity = customerItems.get(item);
 						availability = storeProducts.get(item);
-						System.out.println("pre" + customerItems.get(item));
+
 						quantityMatchCheck();
 						itemQuantityCheck(availability, itemQuantity);
+
+						if (availability < itemQuantity)
+							itemQuantity = 0;
 						availability = availability - itemQuantity;
 						itemQuantity = itemQuantity + quantity;
 						customerItems.put(item, itemQuantity);
@@ -117,10 +113,8 @@ public class ShoppingCart {
 
 					customerInput = choice.inputCheck();
 				}
-
 				out.println("do you want to purchase any other item(yes/no)");
 				customerInput = choice.inputCheck();
-
 				break;
 
 			case "no":
@@ -131,30 +125,28 @@ public class ShoppingCart {
 				break;
 			}
 		} while (customerInput.equalsIgnoreCase("yes"));
-
 	}
 
 	public static void itemQuantityCheck(int availability, int itemQuantity) throws Exception {
 
 		try {
-			if (availability >= itemQuantity) {
+			if ((itemQuantity != 0)) {
+			if ((availability >= itemQuantity)) {
 
 				availability = availability - itemQuantity;
-
-				storeProducts.put(item, availability);
-				customerItems.put(item, itemQuantity);
+				
+					storeProducts.put(item, availability);
+					customerItems.put(item, itemQuantity);
 			} else {
 
 				storeProducts.put(item, availability);
 				throw new Exception();
-			}
+			}}
 		}
-
 		catch (Exception e) {
 			out.println("out of stock\n");
 
 			userItems();
-
 		}
 	}
 
@@ -174,31 +166,32 @@ public class ShoppingCart {
 			}
 		} while (!b);
 	}
-
 	public static void quantityMatchCheck() throws IOException {
 		do {
 			customerInput = "no";
 			try {
 				out.println("enter quantity");
 				checkInteger(itemQuantity);
-
+			} catch (ZeroQuantityException zqe) {
+				out.println("quantity should not zero");
+				out.println("do you want to enter again...(yes/no)");
+				customerInput = choice.inputCheck();
 			} catch (Exception e) {
 				out.println("enter positive digits only");
 				out.println("do you want to enter again...(yes/no)");
 				customerInput = choice.inputCheck();
-
 			}
 
 		} while (customerInput.equals("yes"));
 	}
-
 	public static void checkInteger(Integer quantity) throws Exception {
-		itemQuantity = Integer.parseInt(br.readLine());
-		if (itemQuantity < 0) {
 
+		itemQuantity = Integer.parseInt(br.readLine());
+
+		if (itemQuantity == 0)
+			throw new ZeroQuantityException();
+		if (itemQuantity < 0) {
 			throw new Exception();
 		}
-
 	}
-
 }
